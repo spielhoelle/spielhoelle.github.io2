96db3a8e39c83e0ddaa852fd7af45fb795efea83
Tommy <spielhoelle@posteo.net>
2024-11-11 23:54:26 +0900

    feat: text to binary converter

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
+// first example simple button click
 const buttonEle = document.getElementById("clickEle")
-console.log(buttonEle)
 
 buttonEle.addEventListener("click", function (event) {
-  console.log(event)
-  alert("Yoo this is a button click")
+  const answer = prompt("How old are you?")
+  console.log(answer)
+})
+
+// Second example text to binary
+function textToBinary(str) {
+  let output = ""
+  str.split("").forEach((element) => {
+    let char = element.charCodeAt(0).toString(2)
+    output += ("00000000" + char).slice(-8).concat(" ")
+  })
+  return output
+}
+
+const inputEle = document.getElementById("onTypeEle")
+inputEle.addEventListener("keyup", function (event) {
+  const result = textToBinary(event.target.value)
+  const resultEle = document.getElementById("result")
+  resultEle.innerHTML = result
 })
